377dd141bbabc546e43221840662833f3e17e164
akhileshjain <akhilesh.taurus@gmail.com>
2020-10-07 01:25:03 +0530

    fixed  scroll transition  bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'covid-tracker';
+
+  ngOnInit() {
+    $(function() {
+      var topoffset = 80; //variable for menu height
+    
+      //Use smooth scrolling when clicking on navigation
+      $('.navbar-nav a').click(function() {
+        $('.navbar-toggler').click(); //bootstrap 4.x, close menu on selection of an item.
+
+        if (location.pathname.replace(/^\//,'') === 
+          this.pathname.replace(/^\//,'') && 
+          location.hostname === this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top-topoffset
+            }, 500);
+            return false;
+          }
+        } //click function
+      }); //smooth scrolling
+    
+    }); 
+  }
 }
